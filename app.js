b7813a5dc36a86c559ee6f97336e642c321b030d
erlandasz <erlandasz@gmail.com>
2019-09-24 15:37:37 +0300

    schemas n shit

--- app.js
+++ app.js
@@ -2,38 +2,135 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Event = require('./models/event');
+const User = require('./models/users');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use(
+  '/graphql',
+  graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+            creator: User!
+        }
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+            createdEvent: [Event!]
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        input UserInput{
+            email: String!
+            password: String!
+        }
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
-
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
-
         schema {
             query: RootQuery
             mutation: RootMutation
         }
     `),
     rootValue: {
-        events: () => {
-            return ['walk in a park', 'coding', 'drinking'];
-        },
-        createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+      events: () => {
+        return Event.find()
+          .populate('creator')
+          .then(events => {
+            return events.map(event => {
+              return {
+                ...event._doc,
+                _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          creator: '5d8a0069768a27342465a81f'
+        });
+        let createdEvent;
+        return event
+          .save()
+          .then(result => {
+            createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+            return User.findById('5d8a0069768a27342465a81f');
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error ('User not found...');
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      },
+        createUser: args => {
+          return User.findOne({ email: args.userInput.email})
+          .then(user => {
+            if(user) {
+              throw new Error('User already exists..');
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id};
+          })
+          .catch(err => {
+            throw err;
+          });
         }
     },
     graphiql: true
-}));
+  })
+);
 
-app.listen(3000);
+mongoose.connect(`mongodb+srv://sachs-clone-web:1UnJebPm7GrRK767@cluster0-hk0hh.mongodb.net/sachs-playground?retryWrites=true&w=majority`)
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
 
 
